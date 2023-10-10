7320225480f5853dbdb12b61a6baf9fe31e68f91
Krish Kohli <krishkohli700@gmail.com>
2023-10-10 18:06:01 +0530

    first commit

---
+++ hello app/hello.java
@@ -0,0 +1,17 @@
+class hello
+{
+    public static void main(String[] args)
+    {
+    int i,j;
+    System.out.println("pattern is : ");
+    System.out.println();
+    for( i=1;i<=4;i++)
+    {
+        for(j=1;j<=4;j++)
+        {
+        System.out.print("*");
+        }
+        System.out.println();
+    }
+    }
+}
